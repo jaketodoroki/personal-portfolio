9cc84a1d8a8b6748d5f0a81ef25ae503d55c2057
JakeTodoroki <taco7897@gmail.com>
2023-03-29 15:23:33 -0600

    footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Instagram } from '@mui/icons-material';
+import { LinkedIn } from '@mui/icons-material';
+import { GitHub } from '@mui/icons-material';
+import '../styles/Footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='socialMedia'>
+        <Instagram />
+        <LinkedIn />
+        <GitHub />
+      </div>
+      <p> &copy; 2022 JakeTodoroki.com</p>
+    </div>
   )
 }
 
