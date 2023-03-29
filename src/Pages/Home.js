4825187758ab2b9bedc3dab233033e7627a179b7
JakeTodoroki <taco7897@gmail.com>
2023-03-29 15:43:59 -0600

    home info and style

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,47 @@
 import React from 'react'
+import { Email } from '@mui/icons-material'
+import { LinkedIn } from '@mui/icons-material'
+import { GitHub } from '@mui/icons-material'
+import '../styles/Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='about'>
+        <h2> Hi! My name is Jake.</h2>
+        <div className='prompt'>
+          <p> 
+            An ambitious software developer from Denver, Colorado.
+          </p>
+          <Email />
+          <LinkedIn />
+          <GitHub />
+        </div>
+      </div>
+      <div className='skills'> 
+        <h1> Skills </h1>
+        <ol className='list'>
+          <li className='item'>
+            <h2> Front-End </h2>
+            <span>
+              ReactJS, HTML, CSS, NPM
+            </span>
+          </li>
+          <li className='item'>
+            <h2> Backend-End </h2>
+            <span>
+              NodeJs, MongoDB, JSON API, PostgreSQL
+            </span>
+          </li>
+          <li className='item'>
+            <h2> Languages </h2>
+            <span>
+              JavaScript, Python, Typescript, EJS
+            </span>
+          </li>
+        </ol>
+      </div>
+    </div>
   )
 }
 
